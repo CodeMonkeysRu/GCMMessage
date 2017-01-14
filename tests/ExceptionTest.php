d05d88f21667918a2a050cde4e2edca39a07aa0d
dommccarty <dominic.mccarty@gmail.com>
2017-01-13 23:50:47 -0800

    Add files via upload

---
+++ tests/ExceptionTest.php
@@ -0,0 +1,34 @@
+<?php
+namespace CodeMonkeysRu\GCM;
+
+/**
+ * Class ExceptionTest
+ */
+class ExceptionTest extends \PHPUnit_Framework_TestCase
+{
+
+    /**
+     * @var \CodeMonkeysRu\GCM\Exception
+     */
+
+    private $exception;
+
+    protected function setUp()
+    {
+        $exception = new \CodeMonkeysRu\GCM\Exception("Unknown error.", Exception::UNKNOWN_ERROR);
+        $exception->setMustRetry(true);
+        $exception->setWaitSeconds(120);
+
+        $this->exception = $exception;
+    }
+    
+    public function testGetMustRetry()
+    {
+        $this->assertEquals(true, $this->exception->getMustRetry());
+    }
+
+    public function testGetWaitSeconds()
+    {
+        $this->assertEquals(120, $this->exception->getWaitSeconds();
+    }
+}
--- tests/ResponseTest.php
+++ tests/ResponseTest.php
@@ -1,20 +1,44 @@
 <?php
+namespace CodeMonkeysRu\GCM;
 
-class ResponseTest extends PHPUnit_Framework_TestCase
+/**
+ * Class ResponseTest
+ */
+class ResponseTest extends \PHPUnit_Framework_TestCase
 {
 
     /**
-     *
      * @var \CodeMonkeysRu\GCM\Response
      */
-    private $response;
+    private $responseOK;
+    private $responseHeadersOK;
+
+    private $responseMustRetry;
+    private $responseHeadersMustRetry;
+    
+    private $responseInvalidDataKey;
+    private $responseHeadersInvalidDataKey;
 
     protected function setUp()
     {
-        $message = new \CodeMonkeysRu\GCM\Message(array(1, 2, 3, 4, 5, 6));
-        $responseBody = '{ "multicast_id": 216,
+        $messageOK = new \CodeMonkeysRu\GCM\Message(array(1, 2, 3, 4, 5, 6, 7));
+        
+        $responseHeadersOK = ["HTTP\/1.1 200 OK",
+        "Content-Type: application\/json; charset=UTF-8",
+        "Date: Sat, 14 Jan 2017 05:32:39 GMT",
+        "Expires: Sat, 14 Jan 2017 05:32:39 GMT",
+        "Cache-Control: private, max-age=0",
+        "X-Content-Type-Options: nosniff","X-Frame-Options: SAMEORIGIN",
+        "X-XSS-Protection: 1; mode=block",
+        "Server: GSE",
+        "Alt-Svc: quic=\":443\"; ma=2592000; v=\"35,34\"",
+        "Accept-Ranges: none",
+        "Vary: Accept-Encoding",
+        "Transfer-Encoding: chunked"];
+                            
+        $responseBodyOK = '{ "multicast_id": 216,
             "success": 3,
-            "failure": 3,
+            "failure": 4,
             "canonical_ids": 1,
             "results": [
               { "message_id": "1:0408" },
@@ -22,26 +46,66 @@ class ResponseTest extends PHPUnit_Framework_TestCase
               { "error": "InvalidRegistration" },
               { "message_id": "1:1516" },
               { "message_id": "1:2342", "registration_id": "32" },
-              { "error": "NotRegistered"}
+              { "error": "NotRegistered"},
+			  { "error": "MismatchSenderId"}
             ]
           }';
+          
+        $this->responseHeadersOK = $responseHeadersOK;
+        $this->responseOK = new \CodeMonkeysRu\GCM\Response($messageOK, $responseBodyOK, $responseHeadersOK);
+                        
+        $messageInvalidDataKey = new \CodeMonkeysRu\GCM\Message(array(1, 2));
+        $responseBodyInvalidDataKey = '{ "multicast_id": 216,
+            "success": 0,
+            "failure": 2,
+            "canonical_ids": 0,
+            "results": [
+              { "error": "InvalidDataKey" },
+              { "error": "InvalidDataKey" }          
+            ]
+          }';
+          
+        $this->responseInvalidDataKey = new \CodeMonkeysRu\GCM\Response($messageInvalidDataKey, $responseBodyInvalidDataKey, $responseHeadersOK);
+    }
 
-        $this->response = new \CodeMonkeysRu\GCM\Response($message, $responseBody);
+    public function testGetResponseHeaders()
+    {
+        $this->assertEquals($this->responseHeadersOK, $this->responseOK->getResponseHeaders());
     }
 
     public function testGetNewRegistrationIds()
     {
-        $this->assertEquals(array(5 => 32), $this->response->getNewRegistrationIds());
+        $this->assertEquals(array(5 => 32), $this->responseOK->getNewRegistrationIds());
     }
 
     public function testGetInvalidRegistrationIds()
     {
-        $this->assertEquals(array(3, 6), $this->response->getInvalidRegistrationIds());
+        $this->assertEquals(array(3, 6), $this->responseOK->getInvalidRegistrationIds());
     }
 
     public function testGetUnavailableRegistrationIds()
     {
-        $this->assertEquals(array(2), $this->response->getUnavailableRegistrationIds());
+        $this->assertEquals(array(2), $this->responseOK->getUnavailableRegistrationIds());
     }
-
+    
+    public function testGetExistsMismatchSenderIdYes()
+    {
+        $this->assertEquals(true, $this->responseOK->getExistsMismatchSenderId());
+    }
+    public function testGetExistsMismatchSenderIdNo()
+    {
+        $this->assertEquals(false, $this->responseInvalidDataKey->getExistsMismatchSenderId());
+    }
+    public function testGetMismatchSenderIdIds()
+    {
+        $this->assertEquals(array(7), $this->responseOK->getMismatchSenderIdIds());
+    }
+    public function testGetExistsInvalidDataKeyYes()
+    {
+        $this->assertEquals(true, $this->responseInvalidDataKey->getExistsInvalidDataKey());
+    }
+    public function testGetExistsInvalidDataKeyNo()
+    {
+        $this->assertEquals(false, $this->responseOK->getExistsInvalidDataKey());
+    }
 }
