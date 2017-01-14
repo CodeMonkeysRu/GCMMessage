13a0bc220f2360527f99d75985b18c81fbc0a7c5
dommccarty <dominic.mccarty@gmail.com>
2017-01-14 00:10:29 -0800

    Add files via upload

--- tests/bootstrap.php
+++ tests/bootstrap.php
@@ -1,2 +1,5 @@
 <?php
-require_once __DIR__.'/../vendor/autoload.php';
+require_once __DIR__.'/../library/CodeMonkeysRu/GCM/Exception.php';
+require_once __DIR__.'/../library/CodeMonkeysRu/GCM/Message.php';
+require_once __DIR__.'/../library/CodeMonkeysRu/GCM/Response.php';
+require_once __DIR__.'/../library/CodeMonkeysRu/GCM/Sender.php'; 
